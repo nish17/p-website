2ad87352d75e899246b890939184be17d1de3ad6
Nimesh Solanki <nimesh3536@gmail.com>
2018-08-18 18:59:42 +0530

    few modification

--- public/js/index.js
+++ public/js/index.js
@@ -2,16 +2,26 @@ import React, { Component } from "react";
 import { render } from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import App from "../Components/App.jsx";
+import AboutMe from "../Components/AboutMe.jsx";
+import Resume from "../Components/Resume.jsx";
+import Contact from "../Components/Contact.jsx";
+import NavBar from "../Components/NavBar.jsx";
 import Page404 from "../Components/Page404.jsx";
 
 class Home extends Component {
   render() {
     return (
       <Router>
-        <Switch>
-          <Route exact path="/" component={App} />
-          <Route component={Page404} />
-        </Switch>
+        <div>
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={App} />
+            <Route path="/about" component={AboutMe} />
+            <Route path="/resume" component={Resume} />
+            <Route path="/contact" component={Contact} />
+            <Route component={Page404} />
+          </Switch>
+        </div>
       </Router>
     );
   }
